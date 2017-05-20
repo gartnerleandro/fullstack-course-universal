8e2fabda506c70a9f28c3b7010f5e7b8c0578a17
Leandro Gartner <leandro@analitia.com>
2017-05-20 16:39:13 +0200

    feature: added routes and modified badge

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
@@ -12,18 +13,25 @@ import {addToCart} from './actions/cartActions';
 import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
 
 import BooksList from './components/pages/booksList';
-import Menu from './components/menu';
-import Footer from './components/footer';
+import Cart from './components/pages/cart';
+import BooksForm from './components/pages/booksForm';
+import Main from './main';
 
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
-render(
+const Routes = (
   <Provider store={store}>
-    <div>
-      <Menu />
-      <BooksList />
-      <Footer />
-    </div>
-  </Provider>, document.getElementById('app')
+      <Router history={browserHistory}>
+        <Route path='/' component={Main}>
+          <IndexRoute component={BooksList} />
+          <Route path='/admin' component={BooksForm} />
+          <Route path='/cart' component={Cart} />
+        </Route>
+      </Router>
+  </Provider>
+)
+
+render(
+  Routes, document.getElementById('app')
 );
